7d873868e262710337331d21edcb3f83c670167e
SHO800 <86473181+SHO800@users.noreply.github.com>
2023-09-30 13:42:59 +0900

    file transfer 9/30

--- register/__init__.py
+++ register/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_socketio import SocketIO
-
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 app.config.from_object('register.config')
 socketio = SocketIO(app)
+
 
 db = SQLAlchemy(app)
 
